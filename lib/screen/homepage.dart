174f22226af3ae5248120ecdd0adb5edab3ec285
Candice Ariel <79513595+CandiceAriel@users.noreply.github.com>
2023-11-11 01:13:54 +0700

    add image

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -2,10 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:notosuru/firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:notosuru/screen/newnote.dart';
-import 'package:notosuru/screen/note.dart';
-
+import 'package:flutter/services.dart' show rootBundle;
 import '../firestore.dart';
+
+//load component
+import 'package:notosuru/screen/note.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -43,72 +44,91 @@ class _HomePageState extends State<HomePage> {
             if(snapshot.hasData){
               List notesList = snapshot.data!.docs;
 
-              return ListView.builder(
-                itemCount: notesList.length,
-                itemBuilder: (context, index){
-                  DocumentSnapshot document = notesList[index];
-                  String docID = document.id;
+              if(notesList.length != 0){
+                return ListView.builder(
+                  itemCount: notesList.length,
+                  itemBuilder: (context, index){
+                    DocumentSnapshot document = notesList[index];
+                    String docID = document.id;
 
 
-                  Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-                  String noteTitle = data['title'];
-                  String noteContent = data['content'];
+                    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                    String noteTitle = data['title'];
+                    String noteContent = data['content'];
 
-                  return InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Note(docID: docID, title: noteTitle, content: noteContent)),
-                      );
-                    },
-                    child: Container(
-                      height: 110.0,
-                      margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                      ),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 7,
-                            child: Column(
-                               crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    data['title'],
-                                    style: GoogleFonts.nunitoSans(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.w800
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Note(docID: docID, title: noteTitle, content: noteContent)),
+                        );
+                      },
+                      child: Container(
+                        height: 110.0,
+                        margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 7,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      data['title'],
+                                      style: GoogleFonts.nunitoSans(
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.w800
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    data['content'],
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
+                                  Expanded(
+                                    child: Text(
+                                      data['content'],
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                    )
                                   )
-                                )
-                            ]),
-                          ),
-                          Expanded(
-                            child: IconButton(
-                              onPressed: () => firestoreService.deleteNote(docID), 
-                              icon: const Icon(Icons.delete)
+                              ]),
+                            ),
+                            Expanded(
+                              child: IconButton(
+                                onPressed: () => firestoreService.deleteNote(docID), 
+                                icon: const Icon(Icons.delete)
+                              )
                             )
-                          )
-                        ],
+                          ],
+                        )
+                        
                       )
-                      
-                    )
-                  ); 
-                }
-              );
+                    ); 
+                  }
+                );
+              } else {
+                return const Column(
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/Shrug-bro.png')
+                    ),
+                    Text('Note is empty')
+                  ]
+                );
+              }
+              
             } else {
-              return const Text("No Notes");
+              return const Column(
+                children: [
+                  Image(
+                      image: AssetImage('assets/images/Shrug-bro.png')
+                    ),
+                  Text('Note is empty')
+                ]
+              );
             }
           }
         ),
